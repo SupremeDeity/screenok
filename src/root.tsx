12e0597203ead06525c748e326113a3ed9ac9836
SupremeDeity <29257166+SupremeDeity@users.noreply.github.com>
2024-03-22 17:12:52 +0500

    Add some metadata

--- src/root.tsx
+++ src/root.tsx
@@ -19,6 +19,22 @@ export default component$(() => {
     <QwikCityProvider>
       <head>
         <meta charSet="utf-8" />
+        <title>Screenok</title>
+        <meta
+          name="description"
+          content="Create mockups from your screenshots"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Screenok" />
+        <meta property="og:url" content="https://screenok.vercel.app" />
+        <meta
+          property="og:image"
+          content="https://screenok.vercel.app/og.svg"
+        />
+        <meta
+          property="og:description"
+          content="Generate Screenshot Mockups."
+        />
         <link rel="manifest" href="/manifest.json" />
         <ServiceWorkerRegister />
       </head>
